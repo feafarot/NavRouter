6288c037fabde95c98259d0a43f385e71489f67a
feafarot <sly.feafarot@gmail.com>
2013-12-18 11:50:28 +0200

    Updates script (test)

--- update-pages.ps1
+++ update-pages.ps1
@@ -1,10 +1,16 @@
+Write-Host '~- Running: git checkout gh-pages'
 git checkout gh-pages
+Write-Host '~- Running: git checkout master -- sample'
 git checkout master -- sample
+Write-Host '~- Running: Copy-Item "sample\*" ".\" -recurse - force'
 Copy-Item "sample\*" ".\" -recurse - force
+Write-Host '~- Running: Remove-Item "sample" -recurse'
 Remove-Item "sample" -recurse
+Write-Host '~- Running: git add .'
 git add .
+Write-Host '~- Running: git commit -sm "..."'
 git commit -sm "[Automated] gh-pages updated to latest sample from master branch"
-git remote set-url origin git@github.com:feafarot/NavRouter.git
-git push origin gh-pages:gh-pages
-git remote set-url origin https://github.com/feafarot/NavRouter.git
-git checkout master
+#git remote set-url origin git@github.com:feafarot/NavRouter.git
+#git push origin gh-pages:gh-pages
+#git remote set-url origin https://github.com/feafarot/NavRouter.git
+#git checkout master
