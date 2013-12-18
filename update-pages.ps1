360f53934d0c8e663588264c5feba3ccd6481665
feafarot <sly.feafarot@gmail.com>
2013-12-18 11:40:17 +0200

    Updated script (test)

--- update-pages.ps1
+++ update-pages.ps1
@@ -4,5 +4,7 @@ Copy-Item "sample\*" ".\" -recurse - force
 Remove-Item "sample" -recurse
 git add .
 git commit -sm "[Automated] gh-pages updated to latest sample from master branch"
+git remote set-url origin git@github.com:feafarot/NavRouter.git
 git push origin gh-pages:gh-pages
+git remote set-url origin https://github.com/feafarot/NavRouter.git
 git checkout master
