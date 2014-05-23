83f6933207eb385fc681d02b34c31ab61a12f270
feafarot <sly.feafarot@gmail.com>
2014-05-23 13:46:45 +0300

    [Automated] gh-pages updated to latest sample from master branch
    Signed-off-by: feafarot <sly.feafarot@gmail.com>

---
+++ Scripts-App/viewModels.js
@@ -0,0 +1,91 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var viewModels;
+(function (viewModels) {
+    var BaseVM = (function () {
+        function BaseVM() {
+            for (var i in this) {
+                if (!this.hasOwnProperty(i) && typeof (this[i]) === "function" && i != "constructor") {
+                    this[i] = this[i].bind(this);
+                }
+            }
+        }
+        return BaseVM;
+    })();
+    viewModels.BaseVM = BaseVM;
+
+    var Page1VM = (function (_super) {
+        __extends(Page1VM, _super);
+        function Page1VM() {
+            _super.call(this);
+            this.num = ko.observable("0");
+            this.confirmationVisible = ko.observable(false);
+            this.forPayload = ko.observable({ text: "Hello from payload!" });
+        }
+        Page1VM.prototype.showMagicNumber = function () {
+            this.num(Math.floor(Math.random() * 100).toString());
+        };
+
+        Page1VM.prototype.canNavigateFrom = function (callback) {
+            this.currentCallback = callback;
+            this.confirmationVisible(true);
+        };
+
+        Page1VM.prototype.confirmYes = function () {
+            this.currentCallback(true);
+            this.confirmationVisible(false);
+        };
+
+        Page1VM.prototype.confirmNo = function () {
+            this.currentCallback(false);
+            this.confirmationVisible(false);
+        };
+        return Page1VM;
+    })(BaseVM);
+    viewModels.Page1VM = Page1VM;
+
+    var Page2VM = (function (_super) {
+        __extends(Page2VM, _super);
+        function Page2VM() {
+            _super.call(this);
+            this.num = ko.observable("0");
+            this.params = ko.observable({});
+        }
+        Page2VM.prototype.showMagicNumber = function () {
+            this.num("P2:" + Math.floor(Math.random() * 100));
+        };
+
+        Page2VM.prototype.onNavigatedTo = function (params, payload) {
+            this.params(params);
+        };
+        return Page2VM;
+    })(BaseVM);
+    viewModels.Page2VM = Page2VM;
+
+    var Page3VM = (function (_super) {
+        __extends(Page3VM, _super);
+        function Page3VM() {
+            _super.call(this);
+            this.num = ko.observable("0");
+            this.params = ko.observable({});
+            this.payload = ko.observable(null);
+        }
+        Page3VM.prototype.showMagicNumber = function () {
+            this.num("P3:" + Math.floor(Math.random() * 11));
+        };
+
+        Page3VM.prototype.onNavigatedTo = function (params, payload) {
+            this.payload(ko.toJSON(payload));
+        };
+
+        Page3VM.prototype.onNavigatedFrom = function () {
+            alert("Page 3 Left");
+        };
+        return Page3VM;
+    })(BaseVM);
+    viewModels.Page3VM = Page3VM;
+})(viewModels || (viewModels = {}));
---
+++ Scripts-App/viewModels.ts
@@ -0,0 +1,100 @@
+﻿module viewModels
+{
+    export class BaseVM
+    {
+        constructor()
+        {
+            for (var i in this)
+            {
+                if (!this.hasOwnProperty(i) && typeof (this[i]) === "function" && i != "constructor")
+                {
+                    this[i] = this[i].bind(this);
+                }
+            }
+        }
+    }
+
+    export class Page1VM extends BaseVM implements routing.routes.INavigationAware
+    {
+        currentCallback: any;
+        num: KnockoutObservable<string> = ko.observable("0");
+        confirmationVisible: KnockoutObservable<boolean> = ko.observable(false);
+        forPayload: KnockoutObservable<any> = ko.observable({ text: "Hello from payload!" });
+
+        constructor()
+        {
+            super();
+        }
+
+        showMagicNumber(): void
+        {
+            this.num(Math.floor(Math.random() * 100).toString());
+        }
+
+        canNavigateFrom(callback: (allow: boolean) => void): void
+        {
+            this.currentCallback = callback;
+            this.confirmationVisible(true);
+        }
+
+        confirmYes(): void
+        {
+            this.currentCallback(true);
+            this.confirmationVisible(false);
+        }
+
+        confirmNo(): void
+        {
+            this.currentCallback(false);
+            this.confirmationVisible(false);
+        }
+    }
+
+    export class Page2VM extends BaseVM implements routing.routes.INavigationAware
+    {
+        num: KnockoutObservable<string> = ko.observable("0");
+        params: KnockoutObservable<any> = ko.observable({});
+
+        constructor()
+        {
+            super();
+        }
+
+        showMagicNumber(): void
+        {
+            this.num("P2:" + Math.floor(Math.random() * 100));
+        }
+        
+        onNavigatedTo(params, payload): void
+        {
+            this.params(params);
+        }
+    }
+
+    export class Page3VM extends BaseVM implements routing.routes.INavigationAware
+    {
+        num: KnockoutObservable<string> = ko.observable("0");
+        params: KnockoutObservable<any> = ko.observable({});
+        payload: KnockoutObservable<any> = ko.observable(null);
+
+        constructor()
+        {
+            super();
+        }
+
+        showMagicNumber(): void
+        {
+            this.num("P3:" + Math.floor(Math.random() * 11));
+        }
+
+        onNavigatedTo(params, payload): void
+        {
+            this.payload(ko.toJSON(payload));
+        }
+
+        onNavigatedFrom(): void
+        {
+            alert("Page 3 Left");
+        }
+    }
+}
