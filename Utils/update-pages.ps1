d1bd6a4ffa5399b6739f5a842b4421b0a4f8e08c
feafarot <sly.feafarot@gmail.com>
2014-05-23 13:53:43 +0300

    Update pages moved and fixed

--- Utils/update-pages.ps1
+++ Utils/update-pages.ps1
@@ -4,11 +4,12 @@ git checkout gh-pages
 Write-Host 'Obtaining sample from master branch...'
 git checkout master sample
 
-Write-Host "Copying files from 'sample' to root folder..."
-Copy-Item "sample\*" ".\" -recurse -force
+Write-Host "Copying files from 'Sample' and 'Compiled' to root folder..."
+Copy-Item "Sample\*" ".\" -recurse -force
+Copy-Item "Compiled\*" ".\" -recurse -force
 
-Write-Host "Removing 'sample' folder..."
-Remove-Item "sample" -recurse
+Write-Host "Removing 'Sample' folder..."
+Remove-Item "Sample" -recurse
 
 Write-Host "Indexing of changes in git..."
 git add .
@@ -21,10 +22,13 @@ git commit -sm "[Automated] gh-pages updated to latest sample from master branch
 #git push origin gh-pages:gh-pages
 #git remote set-url origin https://github.com/feafarot/NavRouter.git
 
+Write-Host "Pushing 'gh-pages' into origin"
+git push origin gh-pages:gh-pages
+
 Write-Host "Returning to 'master' branch..."
 git checkout master
 
-Write-Host "Restorin removed files..."
-git reset --hard
+#Write-Host "Restorin removed files..."
+#git reset --hard
 
 Write-Host "Updating 'gh-pages' ended."
